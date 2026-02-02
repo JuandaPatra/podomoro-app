9a82e2d2d1b8a455c4102895e2b8eac763bc8734
Juanda Patra <rahmad.patra@metrocomjaddi.com>
2026-02-02 11:10:09 +0700

    add timer for 5s

--- src/hooks/useTimer.js
+++ src/hooks/useTimer.js
@@ -14,7 +14,12 @@ export const useTimer = (initialMinutes = 25) => {
     } else if (timeLeft === 0 && isRunning) {
       setIsRunning(false);
       const audio = new Audio("https://actions.google.com/sounds/v1/alarms/beep_short.ogg");
+      audio.loop = true;
       audio.play().catch(e => console.error("Audio play failed", e));
+      setTimeout(() => {
+        audio.pause();
+        audio.currentTime = 0;
+      }, 5000);
     }
     return () => clearInterval(interval);
   }, [isRunning, timeLeft]);
